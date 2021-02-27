380a7619ccb573c516e99be3142139a0db4805a5
khazarkhorrami <33454475+khazarkhorrami@users.noreply.github.com>
2021-02-27 23:06:11 +0200

    Add files via upload

--- alignment_scores.py
+++ alignment_scores.py
@@ -9,29 +9,30 @@ import scipy.io
 import cv2
 import os
 
-path_project = '/worktmp/hxkhkh/project2/'
-path_in_corrected_ind = os.path.join(path_project , 'outputs/step_6/step_2/')
+path_project = '/worktmp/hxkhkh/project_3/'
 
+path_in_metadata  = os.path.join(path_project , 'outputs/step_0/coco_validation/')
+file_in_metadata = 'processed_data_list_val.mat'
 
-path_in_AVtensor = os.path.join(path_project ,'outputs/step_5/hidden/')
+path_in_corrected_ind = os.path.join(path_project , 'outputs/step_1/3/')
+file_in_corrected_ind = 'corrected_nouns_index.mat'
+
+path_in_labels = os.path.join(path_project , 'outputs/step_1/4/')
+file_in_labels = 'unified_labels.mat'
+
+path_in_processed_nouns = os.path.join(path_project , 'outputs/step_1/6/')
+file_in_processed_nouns = 'sub_labels.mat'
+
+# path_in_processes_time = os.path.join(path_project ,'outputs/step_6/step_6/' )
+# file_in_processes_time = 'sub_times.mat'
+
+path_in_AVtensor = os.path.join('/worktmp/hxkhkh/project2/' ,'outputs/step_5/hidden/')
 file_in_AVtensor = 'SI_CNN2_v2'
 
 
-path_in_metadata  = os.path.join(path_project , 'outputs/step_1/validation/')
-file_in_metadata = 'processed_data_list_val.mat'
 
-
-path_in_processed_nouns = '/worktmp/hxkhkh/project2/outputs/step_6/step_5/'
-file_in_processed_nouns = 'sub_labels.mat'
-
-path_in_processes_time = os.path.join(path_project ,'outputs/step_6/step_6/' )
-file_in_processes_time = 'sub_times.mat'
-
-path_in_labels = os.path.join(path_project ,'outputs/step_6/step_4/')
-file_in_labels = 'unified_labels.mat'
-
-path_out = '/worktmp/hxkhkh/project3/outputs/step_2/'
-file_out = 'alignments' + file_in_AVtensor 
+path_out = os.path.join(path_project , 'outputs/step_2/')
+file_out = 'alignments_' + file_in_AVtensor 
 #.............................................................................. input parameters
 
 res_target_h = 224    
@@ -148,7 +149,7 @@ def compute_temporal_alignment (t_on, t_off,tensor_AV_image, imID):
                 
 #.............................................................................. loading input files
 
-data = scipy.io.loadmat(path_in_corrected_ind + 'corrected_nouns_index.mat', variable_names = ['ind_accepted'])
+data = scipy.io.loadmat(path_in_corrected_ind + file_in_corrected_ind , variable_names = ['ind_accepted'])
 ind_accepted = data['ind_accepted'][0]
 number_of_images = len(ind_accepted)
 #.....................
@@ -171,9 +172,12 @@ all_image_ids = [all_image_ids[item] for item in ind_accepted]
 
 #.....................
 
-data = scipy.io.loadmat(path_in_processed_nouns + file_in_processed_nouns, variable_names = ['all_accpted_words','all_accepted_ind'])
+data = scipy.io.loadmat(path_in_processed_nouns + file_in_processed_nouns, variable_names = ['all_accpted_words','all_accepted_ind','all_accpted_onsets','all_accpted_offsets'])
 all_nouns = data ['all_accpted_words'][0]
 all_inds = data ['all_accepted_ind'][0]
+all_onsets = data ['all_accpted_onsets'][0]
+all_offsets = data ['all_accpted_offsets'][0]
+
 number_of_items = 0
 temp = all_nouns
 all_nouns = []
@@ -186,13 +190,6 @@ for imitem in temp:
             number_of_items += 1
     all_nouns.append(subnouns_im)        
 del temp
-
-#.....................
-
-data = scipy.io.loadmat(path_in_processes_time + file_in_processes_time , variable_names = ['wavfile_nouns_onsets_sim','wavfile_nouns_offset_sim','all_time_masks'])
-
-all_onsets = data ['wavfile_nouns_onsets_sim'][0]
-all_offsets = data ['wavfile_nouns_offset_sim'][0]
 
 #.....................
 
@@ -231,7 +228,8 @@ all_sa_scores =  numpy.zeros([80, 1])
 all_ta_scores =  numpy.zeros([80, 1])
 all_meta_info =  numpy.zeros([80, 3])
 
-
+allrand_sa_scores =  numpy.zeros([80, 1])
+allrand_ta_scores =  numpy.zeros([80, 1])
 ##############################################################################
 
                                     # MAIN
@@ -243,6 +241,9 @@ tensor_input_SA = numpy.reshape(tensor_input_SA, [tensor_input.shape[0], res_sou
 
 tensor_input_TA = softmax_temporal(tensor_input)
 tensor_input_TA = numpy.reshape(tensor_input_TA, [tensor_input.shape[0], res_source_t , res_source_h, res_source_w])
+
+
+
 
 for counter_image in range(number_of_images):
     
@@ -256,6 +257,8 @@ for counter_image in range(number_of_images):
     
     tensor_in_ta  = tensor_input_TA[counter_image]
     tensor_in_ta = upsample_3D (tensor_in_ta, scale_t , scale_h, scale_w)
+    
+    tensor_rand = numpy.random.rand(512,224,224)
     
     subinds = all_inds[counter_image]   
     ref_names = all_reference_names[counter_image]    
@@ -274,19 +277,28 @@ for counter_image in range(number_of_images):
             if subnoun in ref_names: 
                 
                 #..............................................................................  Spatial Alignment
-                t_onset = t_onsets_image[counter_label]
-                t_offset = t_offsets_image[counter_label]
-                t_duration = t_offset- t_onset
+                t_onset_sa = int(t_onsets_image[counter_label])
+                t_offset_sa = int(t_offsets_image[counter_label])
+                t_duration = t_offset_sa - t_onset_sa
                 
-                score_sa, area_GT , col = compute_spatial_alignment (t_onset, t_offset,tensor_in_sa, imageID)
+                score_sa, area_GT , col = compute_spatial_alignment (t_onset_sa, t_offset_sa ,tensor_in_sa, imageID)
+                print(score_sa)
+                
+                scorerand_sa, arearand_GT , col = compute_spatial_alignment (t_onset_sa, t_offset_sa ,tensor_rand, imageID)
                 
                 #.............................................................................. Temporal Alignment
-                t_onset = numpy.maximum (t_onsets_image[counter_label] - 50 , 0 )
-                t_offset = numpy.minimum (t_offsets_image[counter_label]+ 50 , 512)
+                t_onset = int(numpy.maximum (t_onsets_image[counter_label] - 50 , 0 ))
+                t_offset = int(numpy.minimum (t_offsets_image[counter_label]+ 50 , 512))
                 
                 t_duration_extra = t_offset- t_onset
                 
                 score_ta = compute_temporal_alignment (t_onset, t_offset,tensor_in_ta, imageID)
+                print(score_ta)
+                
+                scorerand_ta = compute_temporal_alignment (t_onset, t_offset, tensor_rand, imageID)
+                
+                
+                #.............................................................................. Saving results
                 
                 all_ta_scores[col,0] = all_ta_scores[col,0] + score_ta
                 all_sa_scores[col,0] = all_sa_scores[col,0] + score_sa
@@ -295,7 +307,12 @@ for counter_image in range(number_of_images):
                 all_meta_info [col,1] = all_meta_info [col,1] + t_duration
                 all_meta_info [col,2] = all_meta_info [col,2] + 1
                 
+                allrand_ta_scores[col,0] = allrand_ta_scores[col,0] + scorerand_ta
+                allrand_sa_scores[col,0] = allrand_sa_scores[col,0] + scorerand_sa
                 
-scipy.io.savemat(path_out + file_out , {'all_sa_scores':all_sa_scores, 'all_ta_scores':all_ta_scores, 'all_meta_info':all_meta_info})                
+scipy.io.savemat(path_out + file_out + '.mat' , {'all_sa_scores':all_sa_scores, 'all_ta_scores':all_ta_scores, 
+                                                 'allrand_ta_scores': allrand_ta_scores, 'allrand_sa_scores': allrand_sa_scores ,
+                                                 'all_meta_info':all_meta_info})                
                 
-                
+# from matplotlib import pyplot as plt
+# plt.imshow(numpy.sum (tensor_in_ta[t_onset:t_offset , :, : ] , axis = 0 ))                
