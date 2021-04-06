611151f72dc92a624b46a271bb6cfea8b2106243
khazarkhorrami <33454475+khazarkhorrami@users.noreply.github.com>
2021-04-06 11:59:15 +0300

    Add files via upload

--- testdata_setup/4_unifying_GT_labels.py
+++ testdata_setup/4_unifying_GT_labels.py
@@ -44,7 +44,7 @@ import matplotlib.pyplot as plt
 import pylab
 pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 
-dataDir='/worktmp/khorrami/work/data/MSCOCO/'
+dataDir='/.../MSCOCO/'
 dataType='val2014'
 annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
 anncaptionFile = '{}/annotations/captions_{}.json'.format(dataDir,dataType)
