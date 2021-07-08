ac99e0820d5db7e7f2c927d3a6d953471d3807f6
khazarkhorrami <33454475+khazarkhorrami@users.noreply.github.com>
2021-07-08 13:42:27 +0300

    Add files via upload

---
+++ model/configuration.py
@@ -0,0 +1,32 @@
+
+###################### initial configuration  #################################
+
+
+paths = {
+  "model_name": "CNNatt",
+  "modeldir": "",
+  "featuredir": "",
+  "visual_feature_name" : "vggb5conv3_",
+  "audio_feature_name" : "logmel_"
+}
+
+
+model_settings = {
+  "use_pretrained": False,
+  "training_mode": True,
+  "evaluating_mode": True,
+  "saving_mode":True,
+  "save_best_recall" : False,
+  "save_best_loss" : True,
+  "find_recall" : True,
+  "number_of_epochs" : 200,
+}
+
+feature_settings = {
+    "n_caps_per_image":5,
+    "set_of_train_files":['train_ch0', 'train_ch1', 'train_ch2', 'train_ch3', 'train_ch4', 'train_ch5', 'train_ch6', 'train_ch7'],
+    "set_of_validation_files" : ['train_ch8'],
+    "length_sequence" : 512,
+    "Xshape" : (512,40),
+    "Yshape" : (14,14,512)
+    }
